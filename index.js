ebbef200edcfdf921803c3bc4493ea65b596c1c8
sserges <agounkeserge@gmail.com>
2022-12-11 16:42:27 -0400

    Request Body Data

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const PORT = process.env.PORT || 5001
 
 const app = express()
 
+// Enable body parser
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+)
+
 app.use('/openai', require('./routes/openaiRoutes'))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
